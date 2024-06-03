1c944855879266ad1cd672cc63fb3443bc1f3909
Ben Martin <ben.martin@sky.uk>
2024-06-03 21:34:19 +0100

    Create dummy module

---
+++ modules/dummy/main.tf
@@ -0,0 +1,4 @@
+resource "nomad_job" "dummy" {
+  jobspec = file("${path.module}/jobspec.json")
+  json = true
+}
---
+++ prod/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    nomad = {
+      source = "hashicorp/nomad"
+      version = "2.2.0"
+    }
+  }
+}
+
+provider "nomad" {
+  address = "http://hestia.lan:4646"
+}
