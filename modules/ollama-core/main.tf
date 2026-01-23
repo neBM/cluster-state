54759ac1f85078ad4118ecfdfc3507fa2f1d6cb7
Ben Martin <ben@brmartin.co.uk>
2026-01-23 16:09:36 +0000

    chore: remove unused Nomad modules after K8s migration
    Removed modules for services that have been migrated to Kubernetes:
    - appflowy, forgejo, gitlab, gitlab-runner, keycloak
    - matrix, minio, nextcloud, nginx-sites
    - ollama, ollama-core, open-webui, overseerr
    - plextraktsync, renovate, restic-backup
    - searxng, vaultwarden
    - _shared (unused helper module)
    
    Remaining Nomad modules:
    - elk (Elasticsearch cluster)
    - media-centre (Plex, Sonarr, Radarr)
    - jayne-martin-counselling (static site)
    - nomad-job (base module for Nomad jobs)
    - plugin-csi, plugin-csi-glusterfs (CSI plugins)

--- modules/ollama-core/main.tf
+++
@@ -1,3 +0,0 @@
-resource "nomad_job" "ollama_core" {
-  jobspec = file("${path.module}/jobspec.nomad.hcl")
-}
--- modules/restic-backup/main.tf
+++
@@ -1,3 +0,0 @@
-resource "nomad_job" "restic_backup" {
-  jobspec = file("${path.module}/jobspec.nomad.hcl")
-}
