05cd0713cb4d92f55da66dc930311e60f3907ece
Ben Martin <ben@brmartin.co.uk>
2026-02-05 21:19:51 +0000

    deploy: Athenaeum backend 8b1bbc5d - local JWT validation fix

--- modules-k8s/athenaeum/variables.tf
+++ modules-k8s/athenaeum/variables.tf
@@ -7,7 +7,7 @@ variable "namespace" {
 variable "backend_image" {
   description = "Athenaeum backend container image"
   type        = string
-  default     = "registry.brmartin.co.uk/ben/athenaeum/backend:1bb038fd"
+  default     = "registry.brmartin.co.uk/ben/athenaeum/backend:8b1bbc5d"
 }
 
 variable "frontend_image" {
