84c129deb620cc20e1f1e617208b07eedb0f9c9b
Ben Martin <ben@brmartin.co.uk>
2026-02-07 21:02:18 +0000

    deploy(athenaeum): update backend to dba605a9 with search_vector fix

--- modules-k8s/athenaeum/variables.tf
+++ modules-k8s/athenaeum/variables.tf
@@ -7,7 +7,7 @@ variable "namespace" {
 variable "backend_image" {
   description = "Athenaeum backend container image"
   type        = string
-  default     = "registry.brmartin.co.uk/ben/athenaeum/backend:c42153c7"
+  default     = "registry.brmartin.co.uk/ben/athenaeum/backend:dba605a9"
 }
 
 variable "frontend_image" {
