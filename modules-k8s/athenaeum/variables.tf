f4d19c3a0e6218f3a3d64c040704bf5d6814d2c0
Ben Martin <ben@brmartin.co.uk>
2026-02-07 15:23:19 +0000

    deploy(athenaeum): update backend to 597d4569 with MCP session manager lifespan fix

--- modules-k8s/athenaeum/variables.tf
+++ modules-k8s/athenaeum/variables.tf
@@ -7,7 +7,7 @@ variable "namespace" {
 variable "backend_image" {
   description = "Athenaeum backend container image"
   type        = string
-  default     = "registry.brmartin.co.uk/ben/athenaeum/backend:cd4fa074"
+  default     = "registry.brmartin.co.uk/ben/athenaeum/backend:597d4569"
 }
 
 variable "frontend_image" {
