63779aba67bb3b213ec7ad5585455664087d31eb
Ben Martin <ben@brmartin.co.uk>
2026-02-11 16:09:27 +0000

    deploy(athenaeum): update images to f941f919

--- modules-k8s/athenaeum/variables.tf
+++ modules-k8s/athenaeum/variables.tf
@@ -7,13 +7,13 @@ variable "namespace" {
 variable "backend_image" {
   description = "Athenaeum backend container image"
   type        = string
-  default     = "registry.brmartin.co.uk/ben/athenaeum/backend:71622b25"
+  default     = "registry.brmartin.co.uk/ben/athenaeum/backend:f941f919"
 }
 
 variable "frontend_image" {
   description = "Athenaeum frontend container image"
   type        = string
-  default     = "registry.brmartin.co.uk/ben/athenaeum/frontend:71622b25"
+  default     = "registry.brmartin.co.uk/ben/athenaeum/frontend:f941f919"
 }
 
 variable "domain" {
